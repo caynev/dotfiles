24b7a900f7dc9b3281b97640a03accece88257dc
Cayne Vejvoda <c.vejvoda@demv.de>
2023-10-17 03:11:01 +0200

    feat: ✨ Add zsh config

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install -y npm tmux ripgrep tig
+sudo apt-get install -y npm tmux ripgrep tig zsh exa fzf
 
 git clone https://codeberg.org/caynevejvoda/nvim-config ~/.config/nvim
 git clone https://codeberg.org/caynevejvoda/tmux-config ~/.config/tmux
@@ -9,6 +9,19 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 wget https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz -O /tmp/nvim.tar.gz
 sudo mkdir /usr/local/bin/nvim
 sudo tar -xvf /tmp/nvim.tar.gz -C /usr/local/bin/nvim/ --strip-components 1
+
+git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/djui/alias-tips.git ~/.oh-my-zsh/custom/plugins/alias-tips
+git clone https://github.com/Aloxaf/fzf-tab ~/.oh-my-zsh/custom/plugins/fzf-tab
+git clone https://github.com/z-shell/H-S-MW.git ~/.oh-my-zsh/custom/plugins/H-S-MW
+git clone https://github.com/z-shell/zsh-diff-so-fancy.git ~/.oh-my-zsh/custom/plugins/zsh-diff-so-fancy
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
+git clone https://codeberg.org/caynevejvoda/zsh-config.git ~/.config/zsh
+rm -rf ~/.zshrc
+ln -s ~/.config/zsh/zshrc ~/.zshrc
 
 echo "export PATH=\"$PATH:/usr/local/bin/nvim/bin/:/usr/local/py-utils/bin/\"" >> ~/.bashrc
 echo "alias vim=\"nvim\"" >> ~/.bashrc
