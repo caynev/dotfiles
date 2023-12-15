9d35db245c2b9983a5added8ae8994d332a9e495
Cayne Vejvoda <c.vejvoda@demv.de>
2023-12-15 11:37:01 +0100

    Add aliases for ls and path for nvim to bashrc

--- install.sh
+++ install.sh
@@ -8,6 +8,12 @@ wget https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.
 sudo mkdir /usr/local/bin/nvim
 sudo tar -xvf /tmp/nvim.tar.gz -C /usr/local/bin/nvim/ --strip-components 1
 
+tee -a ~/.bashrc << END
+export PATH="$PATH:/usr/local/bin/nvim/bin/"
+alias ll="ls -l"
+alias la="ls -la"
+END
+
 git config --global gpg.format ssh
 git config --global user.signingkey "key::ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINOJk2GCqHvTwe/4W+mwXRist758E1NUIEHIBQVp46Fc c.vejvoda@demv.de"
 git config --global commit.gpgsign true
